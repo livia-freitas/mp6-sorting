6a797ab3180b32abc3e187ac2c85ac2e416871a2
Livia S. Freitas <138071569+livia-freitas@users.noreply.github.com>
2023-11-02 13:36:45 -0500

    Add files via upload

---
+++ SortingTests.java
@@ -0,0 +1,62 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SortingTests {
+
+  /**
+  * Checks if the empty list is handled gracefully
+  */
+  @Test
+  public void emptyListTest(){
+    assertTrue(true);
+  }
+
+  /**
+  * Checks if the singleton list is handled gracefully
+  */
+  @Test
+  public void singletonTest(){
+
+  }
+
+  /**
+  * Checks if the sort works for an already sorted list
+  */
+  @Test
+  public void alredySortedTest(){
+
+  }
+
+  /**
+  * Checks if a half-sorted list is sorted properly
+  */
+  @Test
+  public void halfSortedTest(){
+
+  }
+
+  /**
+  * Checks if a list made of one repeated element is sorted properly
+  */
+  @Test
+  public void repeatedElementTest(){
+
+  } 
+
+  /**
+  * Checks if negative numbers are sorted properly
+  */
+  @Test
+  public void negativeNumbersTest(){
+
+  }
+
+  /**
+   * Checks if zero is handled properly
+   */
+  @Test
+  public void zeroTest(){
+    
+  }
+}

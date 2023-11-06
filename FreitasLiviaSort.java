b38505ff2f371d14619990f32c288ce036b02963
Livia S. Freitas <138071569+livia-freitas@users.noreply.github.com>
2023-11-05 20:50:37 -0600

    FreitasLiviaSort works

--- FreitasLiviaSort.java
+++ FreitasLiviaSort.java
@@ -1,10 +1,18 @@
+package mp6;
 import java.util.Comparator;
 
 public class FreitasLiviaSort implements Sorter {
   
   public <T> void sort(T[] values, Comparator<? super T> order){
-    
+    for(int i = 0; i < values.length; i++){
+      for (int j = 0; j < values.length; j++){
+        if(order.compare(values[i], values[j]) == 1){
+          T temp = values[i];
+          values[i] = values[j];
+          values[j] = temp;
+        }
+      }
+    }
   }
-
 }
 

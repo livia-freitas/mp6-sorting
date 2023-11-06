38eaacd8d1c0df28411140c9d7e16c5377bdc741
Livia S. Freitas <138071569+livia-freitas@users.noreply.github.com>
2023-11-05 19:51:56 -0600

    MergeSort works

--- MergeSort.java
+++ MergeSort.java
@@ -1,9 +1,47 @@
+package mp6;
 import java.util.Comparator;
+import java.util.Arrays;
 
 public class MergeSort implements Sorter {
   
   public <T> void sort(T[] values, Comparator<? super T> order){
-    
+    if(values.length > 1){
+      int mid = values.length/2;
+      T[] lowerHalf = Arrays.copyOfRange(values, 0, mid);
+      T[] upperHalf = Arrays.copyOfRange(values, mid, values.length);
+      
+
+      sort(upperHalf, order);
+      sort(lowerHalf, order);
+
+      int i = 0;
+      int j = 0;
+      int k = 0;
+
+      while ((i < lowerHalf.length) && (j < upperHalf.length)){
+        if (order.compare(lowerHalf[i], upperHalf[j]) == 1){
+          values[k] = lowerHalf[i];
+          i++;
+        } else {
+          values[k] = upperHalf[j];
+          j++;
+        }
+        k++;
+      }
+      
+      while (i < lowerHalf.length){
+        values[k] = lowerHalf[i];
+        i++;
+        k++;
+      }
+
+      while (j < upperHalf.length){
+        values[k] = upperHalf[j];
+        j++;
+        k++;
+      }
+
+    }
   }
 
 }
--- SortTester.java
+++ SortTester.java
@@ -6,12 +6,14 @@ import java.util.Arrays;
 public class SortTester {
 
   Integer[] sorted = {1, 2, 3, 4, 5};
-  Integer[] testArray = {5, 3, 2, 1, 4};
+  Integer[] testArray1 = {5, 3, 2, 1, 4};
+  Integer[] testArray2 = {3, 2, 4, 1, 5};
+  Integer[] testArray3 = {1, 5, 3, 4, 2};
   Integer[] emptyArray = {};
   Integer[] singleton = {1};
   Integer[] alreadySorted = {1, 2, 3, 4, 5};
-  Integer[] halfSorted = {1, 2, 3, 5, 4, 6};
-  Integer[] repetitiveArray = {1, 1, 1, 1, 1};
+  Integer[] halfSorted = {1, 2, 3, 5, 4};
+  Integer[] repetitiveArray = {2, 1, 3, 1, 4};
   Integer[] negativeArray = {-2, -3, -5, -1, -4};
   Integer[] zeroArray = {1, 0, 2};
 
@@ -24,47 +26,60 @@ public class SortTester {
 
   @Test
   public void unsortedTest(){
-    InsertionSorter.sort(testArray, newComparator);
-    assertTrue(Arrays.equals(testArray, sorted));
+    InsertionSorter.sort(testArray1, newComparator);
+    assertTrue(Arrays.equals(testArray1, sorted));
+    MergeSorter.sort(testArray2, newComparator);
+    assertTrue(Arrays.equals(testArray2, sorted));
+    //add tests for the other sorts
   }
   /**
-  * Checks if the empty list is handled gracefully
+  * Checks if the empty array is handled gracefully
   */
   @Test
-  public void emptyListTest(){
-    assertTrue(true);
+  public void emptyArrayTest(){
+    InsertionSorter.sort(emptyArray, newComparator);
+    assertTrue(Arrays.equals(emptyArray, new Integer[0]));
+    MergeSorter.sort(emptyArray, newComparator);
+    assertTrue(Arrays.equals(emptyArray, new Integer[0]));
   }
 
   /**
-  * Checks if the singleton list is handled gracefully
+  * Checks if the singleton array is handled gracefully
   */
   @Test
   public void singletonTest(){
-
+    Integer[] singletonTest = {1};
+    //INSERTION SORT
+    MergeSorter.sort(singleton, newComparator);
+    assertTrue(Arrays.equals(singleton, singletonTest));
   }
 
   /**
-  * Checks if the sort works for an already sorted list
+  * Checks if the sort works for an already sorted array
   */
   @Test
   public void alredySortedTest(){
-
+    MergeSorter.sort(alreadySorted, newComparator);
+    assertTrue(Arrays.equals(alreadySorted, sorted));
   }
 
   /**
-  * Checks if a half-sorted list is sorted properly
+  * Checks if a half-sorted array is sorted properly
   */
   @Test
   public void halfSortedTest(){
-
+    MergeSorter.sort(halfSorted, newComparator);
+    assertTrue(Arrays.equals(halfSorted, sorted));
   }
 
   /**
-  * Checks if a list made of one repeated element is sorted properly
+  * Checks if a array made of one repeated element is sorted properly
   */
   @Test
   public void repeatedElementTest(){
-
+    Integer[] repetitiveTest = {1, 1, 2, 3, 4};
+    InsertionSorter.sort(repetitiveArray, newComparator);
+    assertTrue(Arrays.equals(repetitiveArray, repetitiveTest));
   } 
 
   /**
@@ -72,6 +87,9 @@ public class SortTester {
   */
   @Test
   public void negativeNumbersTest(){
+    Integer[] negativeTest = {-5, -4, -3, -2, -1};
+    MergeSorter.sort(negativeArray, newComparator);
+    assertTrue(Arrays.equals(negativeArray, negativeTest));
 
   }
 
